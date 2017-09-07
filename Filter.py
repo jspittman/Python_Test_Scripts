7de8ea9fafd002ff66c8d844f9f7e36262d3123b
Jeff Pittman <jeff.pittman@gmail.com>
2017-09-06 21:07:11 -0700

    Add files via upload

--- Filter.py
+++ Filter.py
@@ -1,6 +1,3 @@
-import functools
-from functools import reduce # this is for reduce
-
 
 tup_1 = 1, 2, "me"
 tup_2 = tup_1
@@ -22,27 +19,3 @@ list_2_iter = filter(lambda x: x % 2 == 0, list_1)
 list_2 = list(list_2_iter)
 print(list_1)
 print(list_2)
-
-#bug in this code
-'''
-list_3_item = map(lambda x: x *2, list_1)
-list_3 = list(list_3_iter)
-print(list_3)
-'''
-
-
-# reduce -- where is the new list created?
-list_10 = [1,2,3,4]
-reduce(lambda x, y: x + y, list_10)
-print (list_10)
-
-big_list = list(range(1000))
-reduce(lambda x, y: x +y, big_list)
-
-dna = ["aaa","bbb","ccc"]
-
-
-### awesome ###
-a_count = reduce(lambda a, x: a +x.count("a"), dna, 0)
-print (a_count)
-
