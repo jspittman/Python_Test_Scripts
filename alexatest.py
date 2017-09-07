9aaa39de04eae7f4d53a73cc20277ea63d5b1ab4
Jeff Pittman <jeff.pittman@gmail.com>
2017-09-06 21:02:48 -0700

    Add files via upload

---
+++ alexatest.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+import random
+
+quotes = [
+
+"example quote one","example quote two"]
+
+]
+
+def lambda_handler(event, context):
+	print("event.session.app")
